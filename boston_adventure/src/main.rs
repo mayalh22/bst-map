cb45baad54c9bc05ef803e3a712feeb54b28da14
mayalh22 <maya.l.hazarika@gmail.com>
2025-10-04 20:18:53 -0400

    added choose your own adventure and scene

--- boston_adventure/src/main.rs
+++ boston_adventure/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, Write};
+
+struct Scene {
+    description: &'static str,
+    options: Vec(& 'static str, usize)>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("you wake up at fenway.");
+    println!("what do you want to do?");
+    println!("1. take the t to newbury?");
+    println!("2. follow a rat up the street?");
+    println!("3. take the t west to newton?");
+    print!("> Choose 1, 2, or 3: ");
+
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    match input.trim() {
+        "1" => println!("you arrive at newbury. you see a boba shop"),
+        "2" => println!("you find a secret stash of burritos."),
+        "3" => println!("the train gets stuck!."),
+        _ => println!("invalid choice. a pigeon attacks you."),
+    }
 }
