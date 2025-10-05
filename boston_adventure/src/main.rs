0c1706236c97bfbf154a97cb11de32b6708d13b9
mayalh22 <maya.l.hazarika@gmail.com>
2025-10-04 20:23:20 -0400

    added loop

--- boston_adventure/src/main.rs
+++ boston_adventure/src/main.rs
@@ -1,22 +1,28 @@
 use std::io::{self, Write};
 
 fn main() {
-    println!("you wake up at fenway.");
-    println!("what do you want to do?");
-    println!("1. take the t to newbury?");
-    println!("2. follow a rat up the street?");
-    println!("3. take the t west to newton?");
-    print!("> Choose 1, 2, or 3: ");
+    loop {
+        println!("you wake up at fenway.");
+        println!("what do you want to do?");
+        println!("1. take the t to newbury?");
+        println!("2. follow a rat up the street?");
+        println!("3. take the t west to newton?");
+        println!("4. quit");
+        print!("> Choose 1, 2, 3, or 4: ");
+        io::stdout().flush().unwrap();
 
-    io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed to read line");
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    match input.trim() {
-        "1" => println!("you arrive at newbury. you see a boba shop"),
-        "2" => println!("you find a secret stash of burritos."),
-        "3" => println!("the train gets stuck!."),
-        _ => println!("invalid choice. a pigeon attacks you."),
+        match input.trim() {
+            "1" => println!("you arrive at newbury. you see a boba shop"),
+            "2" => println!("you find a secret stash of burritos."),
+            "3" => println!("the train gets stuck!."),
+            "4" => {
+                println!("you decide to quit. goodbye!");
+                break;
+            }, 
+            _ => println!("invalid choice. a pigeon attacks you."),
+        }
     }
 }
